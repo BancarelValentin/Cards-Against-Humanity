41f686edac2ac5444612de29767d4eb0ea852247
Bancarel Valentin @ MBP <bancarel.valentin@gmail.com>
2015-07-03 20:32:13 +0100

    Adding the cards loader currently hard coded but will get data from the internet or other sources later

---
+++ Cards Against Humanity/CardsLoader.h
@@ -0,0 +1,17 @@
+//
+//  CardsLoader.h
+//  Cards Against Humanity
+//
+//  Created by bancarel valentin on 03/07/15.
+//  Copyright (c) 2015 bancarel valentin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WhiteCard.h"
+#import "BlackCard.h"
+
+@interface CardsLoader : NSObject
+
++(NSArray*)getWhiteCards;
++(NSArray*)getBlackCards;
+@end
